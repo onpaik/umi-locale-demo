a1be7353fb9bb7ac2056590dbc30fc9bd3702f5c
paik <unpaikon@gmail.com>
2019-01-08 13:28:43 +0800

    add eslint run build

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -2,17 +2,23 @@
 
 export default config => {
   config.resolve.modules.add('src');
-  // 排除 ejs svg
-  config.module.rules
-    .get('exclude')
-    .exclude.add(/\.ejs$/)
-    .add(/.svg$/)
-    .end();
+
   config.module
     .rule('svg')
     .test(/\.svg$/i)
     .use('svg-sprite-loader')
     .loader(require.resolve('svg-sprite-loader'));
+  // lint build模式lint代码
+  if (process.env.NODE_ENV === 'production') {
+    config.module
+      .rule('lint-rule')
+      .test(/\.js$/)
+      .use('eslint-loader')
+      .loader(require.resolve('eslint-loader'), {
+        formatter: require('eslint-friendly-formatter'),
+        emitWarning: false,
+      });
+  }
   // 增加alias
   config.resolve.alias
     .set('@compose', 'shared/utils/compose')
