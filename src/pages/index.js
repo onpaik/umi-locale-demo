f27fa0930ce5451faaa5c28dca60c2709b0a1055
paik <unpaikon@gmail.com>
2019-01-12 14:25:04 +0800

    use hooks

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,41 +1,21 @@
-import React /* useState */ from 'react';
+import React, { useState, useEffect } from 'react';
 import moment from 'moment';
 import style from './index.less';
 
 const getTime = () => moment().format('YYYY-MM-DD HH:mm:ss');
 
-export default class Demo extends React.Component {
-  state = {
-    time: getTime(),
-  };
-
-  timer = null;
-
-  polling = () => {
-    clearTimeout(this.timer);
-    this.timer = setTimeout(() => {
-      this.setState({
-        time: getTime(),
-      });
+export default () => {
+  const [time, setTime] = useState(getTime());
+  useEffect(() => {
+    setTimeout(() => {
+      setTime(getTime());
     }, 1000);
-  };
-
-  componentDidMount = () => {
-    this.polling();
-  };
-
-  componentDidUpdate = () => {
-    this.polling();
-  };
-
-  render() {
-    const { time } = this.state;
-    return (
-      <>
-        <div className={style.page}>
-          <span>{time}</span>
-        </div>
-      </>
-    );
-  }
-}
+  });
+  return (
+    <>
+      <div className={style.page}>
+        <span>{time}</span>
+      </div>
+    </>
+  );
+};
